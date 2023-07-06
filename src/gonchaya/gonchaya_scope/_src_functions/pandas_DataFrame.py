bad1c50fa75dce657c422dfc7f4aa7f100b90e8a
Alexander Firsov <gonchaya@gifara.ru>
2023-07-06 08:56:47 +0300

    The first working version of the enhanced pandas module.\nПервая работоспособная версия дополненного модуля pandas.

--- src/gonchaya/gonchaya_scope/_src_functions/pandas_DataFrame.py
+++ src/gonchaya/gonchaya_scope/_src_functions/pandas_DataFrame.py
@@ -1,58 +1,23 @@
-#!/bin/python3
-# it's gonchaya.py
+'''
+Требует заполнения
+'''
+# gonchaya/gonchaya_scope/_src_functions/pandas_DataFrame.py
 
-#   *** Классическая предобработка данных ***
-class GonchayaException(Exception):
-    pass
+from types import FunctionType, MethodType
+import pandas as pd
+from gonchaya.gonchaya_scope.exceptions import *
 
-# декоратор для pandas
-# источник идеи https://tirinox.ru/class-decorator/
-def pandas_with_added_functions(cls):
-    class pandas_upd:
-        def __init__(self, *args, **kwargs):
-            self._obj = cls(*args, **kwargs)
-        def __getattribute__(self, s):
-            try:
-                x = super().__getattribute__(s)
-            except AttributeError:
-                pass
-            else:
-                return x
-            
-            attr = self._obj.__getattribute__(s)
-            # это метод?
-            if isinstance(attr, type(self._obj.__init__)):
-                return attr
-            else:
-                # не метод, что-то другое
-                return attr
-        def test(self):
-            self.abirvalg.__annotations__['return'] = 'test'
-            return 'test'
-        def __str__(self):
-            # позволяет использовать print
-            return self._obj.__str__()
-        def __repr__(self):
-            # формирует текстовое представление в функциях display,
-            # интерактивном выводе значения переменной и пр.
-            return self._obj.__repr__()
-        #def __format__(self, format_spec):
-            #if isinstance(format_spec, unicode):
-            #    return unicode(str(self._obj))
-            #else:
-            #    return str(self._obj)
-        #    return str(self._obj)
-        
-        # автоматически не подтягивается индексирование декорированного класса
-        # укажем явно
-        def __getitem__(self, s):
-            return self._obj.__getitem__(s)
-    return NewCls
+def decorator_add_property(method):
+    def wrapper(*args, **kwargs):
+        tmp=method(*args, **kwargs)
+        args[0].dic=None
+        args[0].title=None
+        #print(f'Создание класса. {id(args[0])=} {id(tmp)=}')
+        pd.DataFrame.preprocessing__normalization_of_column_names = MethodType(_preprocessing__normalization_of_column_names, args[0])
+        return tmp
+    return wrapper
 
-
-
-
-def preparing_string_for_column_names(string:str):
+def _preparing_string_for_column_names(string:'Строка, которую надо исправить (str)')->'исправленная строка (str)':
     '''
     Подготовка строк для названий столбцов.
     Данная функция работает со строками:
@@ -65,8 +30,8 @@ def preparing_string_for_column_names(string:str):
       3) переводит названия в нижний регистр;
       4) удаляет начальные и конечные символы подчеркивания из получившихся названий;
       5) удаляет идущие подряд знаки подчеркивания;
-    На входе: строка (str)
-    На выходе: исправленная строка (str)
+    @param string: строка (str), которую надо исправить
+    @return: исправленная строка (str)
     '''
     table_of_replace = {' ': '_', '	': '_tab_', '.': '_dot_', ':': '_colon_'}
     for i in table_of_replace: string=string.replace(i, table_of_replace[i])
@@ -78,7 +43,7 @@ def preparing_string_for_column_names(string:str):
     while string.find('__') != -1: string = string.replace('__', '_')
     return string
 
-def preprocessing__normalization_of_column_names(dataframe, report=None):
+def _preprocessing__normalization_of_column_names(self, report=None):
     '''
     предобработка: нормализация названий столбцов.
     При обнаружении дубликатов названий генерирует исключение, с описанием ситуации.
@@ -90,12 +55,12 @@ def preprocessing__normalization_of_column_names(dataframe, report=None):
     # Вероятность словить 2 одинаковых имени столбца в датафрейме в ближайшем
     # обозримом будующем равна нулю, поэтому проще кинуть исключение, чем писать
     # обрабатывающую логику.
-    if dataframe.columns.nunique() != len(dataframe.columns):
+    if self.columns.nunique() != len(self.columns):
         raise GonchayaException('В датасете присутствуют столбцы с одинаковым\
  именем. Требуется предварительное ручное вмешательство.')
     columns = {}
-    for name in list(dataframe.columns):
-        preparing_string = preparing_string_for_column_names(name)
+    for name in list(self.columns):
+        preparing_string = _preparing_string_for_column_names(name)
         if name != preparing_string:
             if name not in columns:
                 columns[name] = preparing_string
@@ -106,30 +71,9 @@ def preprocessing__normalization_of_column_names(dataframe, report=None):
             else: raise GonchayaException('Автоматическое переименования столбцов\
  сгенерировало два новых имени, которые совпадают между собой. Требуется\
  предварительное ручное вмешательство. "'+str(name)+'"')
-    dataframe = dataframe.rename(columns=columns)
+    dataframe = self.rename(columns=columns)
     if dataframe.columns.nunique() != len(dataframe.columns):
         raise GonchayaException('Автоматическое переименование столбцов\
  сгенерировало имя, которое совпадает с уже используемым. Требуется\
  предварительное ручное вмешательство.')
     return dataframe.rename(columns=columns)
-
-symtab = '	' # в Юпитере вставить символ табуляции проблематично. Записал в переменную
-
-def duplicated__set(dataset:pd.core.frame.DataFrame):
-    '''
-    Возвращает сет индексов строк, являющихся полными дубликатами
-    '''
-    result = []
-    for i , v in enumerate(dataset.duplicated()):
-        if v: result.append(i)
-    return set(result)
-
-def isna(n):
-    if str(type(n)) == "<class 'NoneType'>": return True
-    if str(type(n)) == "<class 'pandas._libs.missing.NAType'>": return True
-    if n == None: return True
-    if n != n: return True
-    return False
-
-if __name__ == '__main__':
-from gonchaya import _gonchaya
